7d0b505676b1e9d2dbef3d29f4e1c34fdbd1b6d5
autopylot <wleonlin@gmail.com>
2019-04-24 19:45:45 -0700

    Remove windows folder

--- windows/test_ps.ps1
+++
@@ -1,8 +0,0 @@
-function Invoke-GitClone($url) {
-  $name = $url.Split('/')[-1].Replace('.git', '')
-  & git clone $url $name | Out-Null
-  Set-Location $name
-}
-
-Invoke-GitClone("https://github.com/Sycnex/Windows10Debloater")
-.\Windows10SysPrepDebloater.ps1 -SysPrep -Debloat
